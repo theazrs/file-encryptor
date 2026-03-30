c26b6313cc33fd09235211150a8391d19b8ee209
Azure_z <azarias.tomas@gmail.com>
2026-03-30 21:11:34 +0200

    Added ASCII shift to characters of the string input by key value input

--- main.cpp
+++ main.cpp
@@ -1,6 +1,29 @@
 #include <iostream>
+#include <fstream>
 using namespace std;
 
-int main() {
+string shifter(string text, int key)
+{
+    string temp = "";
+    for (auto &ch : text)
+    {
+        temp += char(int(ch) + key);
+    }
+    return temp;
+}
+
+int main()
+{
+    string text;
+    int key;
+    cout << "Enter a string" << endl;
+    cin >> text;
+
+    cout << "Enter the key" << endl;
+    cin >> key;
+
+    cout << "The encrypted version is:" << endl;
+    cout << shifter(text, key) << endl;
+
     return 0;
 }
